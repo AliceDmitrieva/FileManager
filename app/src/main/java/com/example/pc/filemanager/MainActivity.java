1dc7d375164a2b9fe9e1ff8264737ccab0b226ed
Unknown <coolalisa@yandex.ru>
2018-03-06 14:07:18 -0800

    Create toolbar and fix errors with opening subfolders
    Create toolbar, change listing strings of file names to listing files, set filepath to toolbar

--- app/src/main/java/com/example/pc/filemanager/MainActivity.java
+++ app/src/main/java/com/example/pc/filemanager/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.pc.filemanager;
 
 import android.content.Context;
 import android.content.Intent;
-import android.os.Environment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -19,27 +21,57 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private ListView listView;
-    private File file;
-    private List<String> fileNameList;
+    private List<File> fileNameList;
     private FileAdapter mAdapter;
+    private Toolbar mToolbar;
 
-    private boolean isClicked;
+    @NonNull
+    public static List<File> getFileListfromSDCard(@NonNull File file) {
+        String state = Environment.getExternalStorageState();
+        List<File> fileList = new ArrayList<>();
+        if (Environment.MEDIA_MOUNTED.equals(state) && file.isDirectory()) {
+            File[] fileArr = file.listFiles();
+            int length = fileArr.length;
+            for (int i = 0; i < length; i++) {
+                File f = fileArr[i];
+                fileList.add(f);
+            }
+        }
+        return fileList;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listView = (ListView) findViewById(R.id.listView);
-        fileNameList = getFileListfromSDCard();
+        mToolbar = findViewById(R.id.toolbar);
+        mToolbar.setNavigationIcon(R.drawable.ic_arrow_white);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        Bundle extras = getIntent().getExtras();
+        File file = extras == null ? null : (File) extras.getSerializable("directory");
+
+        if (file == null) {
+            file = Environment.getExternalStorageDirectory();
+        }
+
+        updateToolbarTitle(file);
+        fileNameList = getFileListfromSDCard(file);
         mAdapter = new FileAdapter(this, R.layout.list_item, fileNameList);
+        checkEmpty(fileNameList);
+
+        listView = findViewById(android.R.id.list);
         listView.setAdapter(mAdapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                String selectedDirectoryString = fileNameList.get(position);
-                File clickedDirectory = new File(selectedDirectoryString);
+                File clickedDirectory = fileNameList.get(position);
                 Intent i = new Intent(MainActivity.this, MainActivity.class);
                 i.putExtra(getString(R.string.extraName), clickedDirectory);
 
@@ -48,31 +80,25 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public List<String> getFileListfromSDCard() {
-            Bundle extras = getIntent().getExtras();
-            File file = extras == null ? null : (File) extras.getSerializable("directory");
-            if (file == null) {
-                file = Environment.getExternalStorageDirectory();
-            }
-                String state = Environment.getExternalStorageState();
-                List<String> fileList = new ArrayList<String>();
-                if (Environment.MEDIA_MOUNTED.equals(state) && file.isDirectory()) {
-                    File[] fileArr = file.listFiles();
-                    int length = fileArr.length;
-                    for (int i = 0; i < length; i++) {
-                        File f = fileArr[i];
-                        fileList.add(f.getName());
-                    }
-                }
-
-        return fileList;
+    public void checkEmpty(List<File> fileList) {
+        if (fileList.size() == 0) {
+            Toast.makeText(MainActivity.this, "Folder is empty!", Toast.LENGTH_SHORT).show();
+        }
     }
 
-    public class FileAdapter extends ArrayAdapter<String> {
-        private List<String> fileList;
+    public void updateToolbarTitle(File file) {
+        mToolbar.setTitle(file.getPath().toString());
+    }
+
+    static class FileHolder {
+        public TextView fileNameView;
+    }
+
+    public class FileAdapter extends ArrayAdapter<File> {
+        private List<File> fileList;
         private Context context;
 
-        public FileAdapter(Context context, int textViewResourceId, List<String> fileList) {
+        public FileAdapter(Context context, int textViewResourceId, List<File> fileList) {
             super(context, textViewResourceId, fileList);
             this.fileList = fileList;
             this.context = context;
@@ -93,14 +119,10 @@ public class MainActivity extends AppCompatActivity {
             } else {
                 fHolder = (FileHolder) view.getTag();
             }
-            String fileName = fileList.get(position);
+            String fileName = fileList.get(position).getName();
             fHolder.fileNameView.setText(fileName);
 
             return view;
         }
-    }
-
-    static class FileHolder {
-        public TextView fileNameView;
     }
 }
